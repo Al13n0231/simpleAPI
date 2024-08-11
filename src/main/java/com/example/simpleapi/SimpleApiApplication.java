da743151603ab7c9b1065f437706cf9a1f3141f6
Al13n0231 <139857884+Al13n0231@users.noreply.github.com>
2024-08-12 00:41:29 +0800

    initial commit

---
+++ src/main/java/com/example/simpleapi/SimpleApiApplication.java
@@ -0,0 +1,20 @@
+package com.example.simpleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@TODO research and write definitions for the following (code in examples) :
+//1. springboot REST controllers
+//2. difference between entity and models in springboot
+//3. difference between DAO and DTO
+//4. create repo and make commit
+
+
+@SpringBootApplication
+public class SimpleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleApiApplication.class, args);
+    }
+
+}
